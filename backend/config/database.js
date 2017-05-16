4a183fb24c7b1c629fe0aaf53434c8ff386b4f1a
carlos philipe <carlosphbahia@gmail.com>
2017-05-16 15:07:58 -0700

    rodando na 3003

--- backend/config/database.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_cuponfacil')
-
-mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório!"
