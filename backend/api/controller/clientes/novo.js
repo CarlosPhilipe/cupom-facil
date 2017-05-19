0ff3e4765ed9a1facf0657b9b87bea1ff63373f5
carlos philipe <carlosphbahia@gmail.com>
2017-05-19 00:44:07 -0700

    cadastrando cliente

--- backend/api/controller/clientes/novo.js
+++ backend/api/controller/clientes/novo.js
@@ -1,21 +1,21 @@
 const Cliente = require('../../model/Cliente');
 module.exports = function novo(req, res) {
-  console.log("NOVO CLiente");
-  console.log(req.params);
-
-
-
   // you can also build, save and access the object with chaining:
-    // Cliente.build({
-    //    cli_nome: 'foo',
-    //    cli_email: 'bar'
-    //  })
-    // .save()
-    // .then(anotherTask => {
-    //   res.send('Sucesso ');
-    // })
-    // .catch(error => {
-    //   // Ooops, do some error-handling
-    //   res.send('Error ');
-    // })
+    Cliente.build({
+       cli_nome: req.body.cli_nome,
+       cli_email: req.body.cli_email,
+       cli_hashsenha: req.body.cli_hashsenha,
+       cli_token_reset_senha:req.body.cli_token_reset_senha,
+       cli_dt_nascimento: req.body.cli_dt_nascimento,
+       cli_sexo: req.body.cli_sexo,
+       cli_ativo: req.body.cli_ativo
+     })
+    .save()
+    .then(anotherTask => {
+      res.send('Sucesso ');
+    })
+    .catch(error => {
+      // Ooops, do some error-handling
+      res.send('Error ');
+    })
 }
--- backend/api/model/Cliente.js
+++ backend/api/model/Cliente.js
@@ -1,10 +1,38 @@
 const connection = require('../../config/database');
 const Cliente = connection.sequelize.define('clientes', {
     cli_nome: {
-      type: connection.Sequelize.STRING
+      type: connection.Sequelize.STRING(70)
     },
     cli_email: {
-      type: connection.Sequelize.STRING
+      type: connection.Sequelize.STRING(70),
+      validate:{
+        isEmail: true
+      }
+    },
+    cli_hashsenha: {
+      type: connection.Sequelize.STRING(45)
+    },
+    cli_token_reset_senha: {
+      type: connection.Sequelize.STRING(45)
+    },
+    cli_dt_nascimento: {
+      type: connection.Sequelize.DATE
+    },
+    cli_sexo: {
+      type: connection.Sequelize.STRING(4)
+    },
+    cli_dt_criacao: {
+      type: connection.Sequelize.DATE,
+      defaultValue: connection.Sequelize.NOW 
+    },
+    cli_dt_ult_alteracao: {
+      type: connection.Sequelize.DATE,
+      defaultValue: connection.Sequelize.NOW
+    },
+    cli_ativo: {
+      type: connection.Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
     }
   },{timestamps: false}
 );
