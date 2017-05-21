542aeb0e556ec6052c5ada0e6f654bf62458f0e6
carlos philipe <carlosphbahia@gmail.com>
2017-05-20 20:20:17 -0700

    Modelo Estabelecimento

---
+++ backend/api/model/Estabelecimento.js
@@ -0,0 +1,57 @@
+// CHAMADA ao mecanismo de conexão
+const connection = require('../../config/database');
+//Model da classe Estabelecimento
+const Estabelecimento = connection.sequelize.define('estabelecimentos', {
+    est_key: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_token_reset_senha: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_nome: {
+      type: connection.Sequelize.STRING(70)
+    },
+    est_email: {
+      type: connection.Sequelize.STRING(70),
+      validate:{
+        isEmail: true
+      }
+    },
+    est_hashsenha: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_logo: {
+      type: connection.Sequelize.STRING(70)
+    },
+    est_cep: {
+      type: connection.Sequelize.STRING(9)
+    },
+    est_bairro: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_rua: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_numero: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_complemento: {
+      type: connection.Sequelize.STRING(45)
+    },
+    est_dt_criacao: {
+      type: connection.Sequelize.DATE,
+      defaultValue: connection.Sequelize.NOW
+    },
+    est_dt_ult_alteracao: {
+      type: connection.Sequelize.DATE,
+      defaultValue: connection.Sequelize.NOW
+    },
+    est_dt_ult_alteracao: {
+      type: connection.Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  },{timestamps: false}
+);
+
+module.exports = Cliente;
