8ad78bbec1ed7bd8b6211bfee8cb136a2790a5d0
carlos philipe <carlosphbahia@gmail.com>
2017-06-29 18:10:30 -0400

    Adicionado o MD5

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,12 +3,13 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const md5 = require('md5');
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 server.listen(port, function() {
-	console.log(`Oi, o BACKEND está rodando na porta ${port}.`)
+	console.log(`Oi, o BACKEND está rodando na porta ${port}.`);
 })
 
 module.exports = server
