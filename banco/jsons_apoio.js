c074f1ff08ed176cf40aee55df3ab73a3b7c2159
carlos philipe <carlosphbahia@gmail.com>
2017-06-29 20:49:59 -0400

    adicionado o jsons de apoio

---
+++ banco/jsons_apoio.js
@@ -0,0 +1,27 @@
+{
+  "cli_nome":"Neto barroncas",
+  "cli_email":"netobarroncassi@gmail.coml",
+  "cli_hashsenha":"lsasdasdaftsgrgfdxcvxc",
+  "cli_token_reset_senha":"",
+  "cli_dt_nascimento":"1994-10-07T00:00:00.000Z",
+  "cli_sexo":"masc",
+  "cli_dt_criacao":null,
+  "cli_dt_ult_alteracao":null,
+  "cli_ativo":true
+}
+
+{
+  "est_token_reset_senha":null,
+  "est_nome":"Pizzaria",
+  "est_email":"pizzariadatataa@gmail.com",
+  "est_hashsenha":"uhbscoahbs",
+  "est_logo":"http://tata.com.br",
+  "est_cep":"69095187",
+  "est_bairro":"cidade nova",
+  "est_rua":"girua",
+  "est_numero":"2",
+  "est_complemento":"quadra304",
+  "est_dt_criacao":"2017-05-25T16:10:59.000Z",
+  "est_dt_ult_alteracao":"2017-05-25T16:10:59.000Z",
+  "est_ativo":true
+}
