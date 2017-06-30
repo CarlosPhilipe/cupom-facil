f7d4d134ab41380c4057c8d5416d52679392e8bf
carlos philipe <carlosphbahia@gmail.com>
2017-06-30 09:59:22 -0400

    Criado o usar cupom

---
+++ backend/api/controller/cupons/usar.js
@@ -0,0 +1,26 @@
+// CHAMADA ao mecanismo de conexão
+const Cupom = require('../../model/Cupom');
+const Util = require('../../model/Util');
+// método chamado no update dados do cliente
+// /cliente/:id
+module.exports = function usar(req, res) {
+
+  var data = Util().getDate();
+  Cupom.update({
+    cup_usado: 1,
+    cup_dt_ult_alteracao: data,
+  },{
+    where: {
+      id: req.params.id
+    }
+  }
+).then(result => {
+  // em caso de sucesso
+  res.send(`{"mensagem":"ok"}`);
+})
+.catch(error => {
+  // em caso de erro
+  res.send(`{"mensagem":"erro","tipo":"${error}"}`);
+})
+
+}
